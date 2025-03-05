97a7a5c0ecb658d0e117e2b3fdb22972afdfdae0
Ronny Bergmann <git@ronnybergmann.net>
2025-03-05 18:08:21 +0100

    Start a notebook to keep all code we want to run somewhere copyable.

---
+++ presentation/all_examples.jl
@@ -0,0 +1,17 @@
+### A Pluto.jl notebook ###
+# v0.20.4
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 7c627d62-f9e2-11ef-028d-291a5c37a421
+begin
+    i = 1;
+    while i <= 4
+        print(i," ")
+	    i +=1
+    end
+end
+
+# ╔═╡ Cell order:
+# ╠═7c627d62-f9e2-11ef-028d-291a5c37a421
